8fe4105d38cad2b56e7c6f8b302adc65c237859e
Adler Gadioli <adlergadioli@gmail.com>
2026-08-18 21:57:51 -0300

    Modificando arquivo .gitignore

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,20 +5,20 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Banco banco = new Banco("Banco UnDF", "001");
+        Banco banco = new Banco("Banco", "001");
 
         Endereco enderecoAdler = new Endereco("DF", "Brasília", "70000-000");
-        enderecoAdler.setBairro("Jardim Botânico");
-        enderecoAdler.setRua("14");
+        enderecoAdler.setBairro("Asa Sul");
+        enderecoAdler.setRua("1");
         enderecoAdler.setNumero("10");
 
-        TitularConta adler = new TitularConta("Adler Gadioli", "065.708.345-12");
-        adler.setDataNascimento(LocalDate.of(2006, 11, 16));
+        TitularConta adler = new TitularConta("Joao Silva", "000.000.000-00");
+        adler.setDataNascimento(LocalDate.of(2000, 1, 10));
         adler.setEmail("adler@gmail.com");
         adler.setTelefone("(61) 99999-9999");
         adler.setEndereco(enderecoAdler);
 
-        TitularConta eduarda = new TitularConta("Eduarda Costa", "22.656.777-89");
+        TitularConta eduarda = new TitularConta("Eduarda Costa", "111.111.111-11");
 
         Conta contaCorrenteAdler = new ContaCorrente(
                 adler,
